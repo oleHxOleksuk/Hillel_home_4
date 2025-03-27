f35c8f90bca2b091e786edef879d0073d40bc52b
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2025-03-27 19:14:31 +0200

    4_1

---
+++ home_4_2.py
@@ -0,0 +1,10 @@
+#lst = [0, 1, 0, 12,0, 3]
+#lst = [0]
+#lst = [1, 0, 13, 0, 0, 0, 5]
+lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+zero_list = []
+for el in lst:
+    zero_list.append(0)
+    lst.remove(0)
+lst.extend(zero_list)
+print(lst)
