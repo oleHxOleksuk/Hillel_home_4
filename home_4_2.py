b8aadbe36c33a9872d316d199c0172af9b32f9ef
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2025-03-27 19:49:00 +0200

    4_2

--- home_4_2.py
+++ home_4_2.py
@@ -1,10 +1,10 @@
-#lst = [0, 1, 0, 12,0, 3]
+lst = [0, 1, 7, 2, 4, 8]
+#lst = [1, 3, 5]
+#lst = [6]
 #lst = [0]
-#lst = [1, 0, 13, 0, 0, 0, 5]
-lst = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
-zero_list = []
+result = 0
 for el in lst:
-    zero_list.append(0)
-    lst.remove(0)
-lst.extend(zero_list)
-print(lst)
+    if lst.index(el) % 2 == 0:
+        result += el
+result = result * lst[-1]
+print(result)
